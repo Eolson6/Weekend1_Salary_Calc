eceae6583b7fcd80ab703f8c44fcba2e7614f3cb
Erin Olson <erineolson15@gmail.com>
2018-12-16 02:55:04 -0600

    created table for employee data and added some css and a picture

--- client.js
+++ client.js
@@ -12,16 +12,14 @@ class Employee {
 let employeeData = []; // store new employee data
 
 $(document).ready(function () { //init jquery
-    $( '#submitButton' ).on( 'click', updateEmployeeList, ); //on click, run " "
+    $( '#submitButton' ).on( 'click', updateEmployeeList ); //on click, run " "
     $('#submitButton').on('click', calcMonthlySalary);  // on click, run " "
+        
     $('#deleteButton').on('click', deleteLastEmployee); //on click, run ""
-
-$('.input-forms').css('float');
 
 }); // end ready now function
 
 function updateEmployeeList (){
-   
     $('#EmployeeList').val();  //clear out data
     
     //create variables from input fields
@@ -30,14 +28,11 @@ function updateEmployeeList (){
     let idNumberIn = $('#idNumberIn').val();
     let jobTitleIn = $('#jobTitleIn').val();
     let annualSalaryIn = ($('#annualSalaryIn').val());
-    //monthlySalary = annualSalaryIn.toFixed(2); //take 2 decimals from input field
-    
 
-    //test to make sure variables and functions work
+    //test to verify function works as expected
     console.log('employee annualSalaryIn:', annualSalaryIn);
-
     
-    //create new employee object from class constructor 
+    //create new employee object from class constructor
     let addEmployee = new Employee(firstNameIn, lastNameIn, idNumberIn, jobTitleIn, annualSalaryIn);
     employeeData.push( addEmployee ); // push new Employee into employee data array
 
@@ -48,7 +43,15 @@ function updateEmployeeList (){
     //test: console.log( 'showing', newEmployee);
 
     //append new employee info to html 
-    $('#employeeList').append('<p>' + newEmployee + '</p>');
+    // $('#employeeList').append('<p>' + newEmployee + '</p>');
+    $('#name-output').append('<p>' + firstNameIn + ' ' + lastNameIn + '</p>');
+    $('#idNumber-output').append('<p>' + idNumberIn + '</p>');
+    $('#job-title-output').append('<p>' + jobTitleIn + '</p>');
+    $('#annual-salary-output').append('<p>' + annualSalaryIn + '</p>');
+
+
+
+
 
    $('#firstNameIn').val( '');
     $('#lastNameIn').val('');
@@ -86,11 +89,14 @@ function deleteLastEmployee(){
 }
 
 
-    
 
-    
-        
 
+
+
+
+
+
+   
 
 
     
